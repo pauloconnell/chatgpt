197fa41767a1f5ead4e6d4e6394a559f5fa55890
Paul OConnell <paul@knowitalls.com>
2024-11-11 11:22:20 -0800

    feat: hooked up google AI API

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { OpenAI } = require('openai');
+//const { OpenAI } = require('openai');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require('dotenv');
 const cors = require('cors');
 
@@ -8,24 +9,51 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+//const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.API_KEYgem);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+   
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/chat", async (req, res) => {
+// open AI chatGPT
+// app.post("/api/chat", async (req, res) => {
+//   const { prompt } = req.body;
+//   try {
+//     const response = await openai.completions.create({
+//       model: "text-davinci-003",
+//       prompt: prompt,
+//       max_tokens: 150,
+//       temperature: 0.7,
+//     });
+//     res.json(response.data.choices[0].text.trim());
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error communicating with OpenAI API");
+//   }
+// });
+
+//google AI
+app.post("/api/chatGem", async (req, res) => {
   const { prompt } = req.body;
   try {
-    const response = await openai.completions.create({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 150,
-      temperature: 0.7,
-    });
-    res.json(response.data.choices[0].text.trim());
+    
+   
+   if (!prompt) prompt  = "Write a story about a magic backpack.";
+   
+   const result = await model.generateContent(prompt);
+   console.log(result.response.text());
+   return result.response.text();
+    
+   
+   
+
+    
+    
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error communicating with OpenAI API");
+    res.status(500).send("Error communicating with AI API", ERROR);
   }
 });
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Chat from './Components/Chat';
-
+import ChatGem from './Components/ChatGem';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <Chat />
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +17,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1> AI providers:</h1>
+
+      <h2> OpenAI ChatGPT</h2>
+      <Chat />
+
+      <h2> Google </h2>
+      <ChatGem />
     </div>
   );
 }
